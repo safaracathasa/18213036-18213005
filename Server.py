64acf1b8f50d0f46626a359ad647eb83a87c048a
Safara Cathasa Riverinda Rijadi <safara.rijadi@gmail.com>
2015-10-05 07:19:05 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,21 +1,86 @@
-import socket               # Import socket module
+import socket
+import sys
+from thread import *
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                 # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-f = open('test.txt','wb')
-s.listen(5)                 # Now wait for client connection.
-while True:
-    c, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    print "Receiving..."
-    l = c.recv(1024)
-    while (l):
-        print "Receiving..."
-        f.write(l)
-        l = c.recv(1024)
-    f.close()
-    print "Done Receiving"
-    c.send('Thank you for connecting')
-    c.close()                # Close the connection
+HOST = ''	# Symbolic name meaning all available interfaces
+PORT = 8888	# Arbitrary non-privileged port
+add = None
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print 'Socket created'
+
+#Bind socket to local host and port
+
+try:
+	s.bind((HOST, PORT))
+except socket.error , msg:
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+	
+print 'Socket bind complete'
+
+
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+	#Sending message to connected client
+	conn.send("Welcome to the server. Type 'help' to show the list of comands or type 'list' to show the list of avalable files\n") #send only takes string
+	
+	#infinite loop so that function do not terminate and thread do not end.
+	while True:
+		
+		#Receiving from client
+		data = conn.recv(1024)
+		print data
+		
+		if not data: 
+			break
+		elif data == 'list' :
+			reply = 'OK...\n' + data + ':\na). a.txt\nb). b.txt\nc). c.txt'
+			conn.sendall(reply)
+		elif data == '1' :
+			fo = open("data1.txt","r+")
+			st = fo.read(20)
+			reply = 'OK..\n' + "opening "+fo.name+"\n.\n.\n.\n.\n"
+			conn.sendall(reply)
+			conn.sendall(st)
+			fo.close()
+		elif data == '2' :
+			fo = open("data2.txt","r+")
+			st = fo.read(20)
+			reply = 'OK..\n' + "opening "+fo.name+"\n.\n.\n.\n.\n"
+			conn.sendall(reply)
+			conn.sendall(st)
+			fo.close()
+		elif data == '3' :
+			fo = open("data3.txt","r+")
+			st = fo.read(20)
+			reply = 'OK..\n' + "opening "+fo.name+"\n.\n.\n.\n.\n"
+			conn.sendall(reply)
+			conn.sendall(st)
+			fo.close()
+		elif data == "client exit" :
+			print 'Disonnected with ' + add[0] + ':' + str(add[1])
+			break
+		else :
+			reply = "wrong command"
+			conn.sendall(reply)
+	
+	#came out of loop
+	conn.close()
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	add = addr
+	
+	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+	start_new_thread(clientthread ,(conn,))
+
+s.close()
